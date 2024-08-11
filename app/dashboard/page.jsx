345158645cb3a66291d4eac9389ff52131d92a17
silencio260 <faruq5840@gmail.com>
2024-08-11 14:13:03 +0100

    Implement Dashboard page and card, table and chart component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import Card from "../ui/card/card"
+import Chart from "../ui/chart/chart"
+import Rightbar from "../ui/rightbar/rightbar"
+import Transactions from "../ui/transactions/transactions"
+import styles from '../ui/dashboard/dashboard.module.css'
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
   )
 }
 
---
+++ app/ui/rightbar/rightbar.jsx
@@ -0,0 +1,10 @@
+import styles from './rightbar.module.css'
+
+
+const Rightbar = () => {
+  return (
+    <div>Rightbar</div>
+  )
+}
+
+export default Rightbar
