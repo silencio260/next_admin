4d645a6a240762f18bd0418f6d73a96bd22f4b12
silencio260 <faruq5840@gmail.com>
2024-08-11 20:31:30 +0100

    Implement login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import styles from '@/app/ui/login/login.module.css'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+
+      <h1>Login</h1>
+
+        <input type="text" placeholder='username'/>
+        <input type="password" placeholder='password'/>
+        <button>
+          Login
+        </button>
+
+      </form>
+    </div>
   )
 }
 
