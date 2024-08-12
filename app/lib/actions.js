7f555b358dc41d454223e948ff83abcaa3e31043
silencio260 <faruq5840@gmail.com>
2024-08-12 20:37:16 +0100

    Implement server action to add new user to db

---
+++ app/lib/actions.js
@@ -0,0 +1,37 @@
+"use server"
+import { revalidatePath } from "next/cache";
+import { Product, User } from "./models";
+import { connectToDB } from "./utils";
+import { redirect } from "next/navigation";
+// import bcrypt from "bcrypt";
+// import { signIn } from "../auth";
+
+export const addUser = async (formData) => {
+    const { username, email, password, 
+        phone, address, isAdmin, isActive 
+    } = Object.fromEntries(formData);
+
+      try {
+        connectToDB();
+
+        const newUser = User({
+            username,
+            email,
+            password,
+            phone,
+            address,
+            isAdmin,
+            isActive,
+        });
+
+        await newUser.save()
+        
+      } catch (error) {
+        console.log(error)
+        throw new Error('Failed to create user!')
+      }
+      revalidatePath("/dashboard/users");
+      redirect("/dashboard/users");
+}
+
+
