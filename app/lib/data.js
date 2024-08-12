7322500065a7e067b04c9f226023f79f65290215
silencio260 <faruq5840@gmail.com>
2024-08-12 19:55:58 +0100

    Implement db queries with search and pagination

---
+++ app/lib/data.js
@@ -0,0 +1,26 @@
+import { User } from "./models"
+import { connectToDB } from "./utils";
+
+export const fetchUsers =  async (q, page) => {
+
+    const regex = new RegExp(q, 'i')
+
+    const ITEM_PER_PAGE = 2
+
+    try {
+
+        connectToDB()
+        const count = await User.find({username: {$regex: regex}}).count()
+        const users = await User.find({username: {$regex: regex}})
+            .limit(ITEM_PER_PAGE)
+            .skip(ITEM_PER_PAGE * (page-1))
+
+        return {count, users};
+        
+    } catch (error) {
+        console.log(error)
+        throw new Error('Failed to fetch users!')
+    }
+}
+
+
