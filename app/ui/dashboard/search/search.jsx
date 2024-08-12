d2fff05f6bc64a22040e608f121fc735466ade10
silencio260 <faruq5840@gmail.com>
2024-08-12 19:58:05 +0100

    intergrate search component with db

--- app/ui/dashboard/search/search.jsx
+++ app/ui/dashboard/search/search.jsx
@@ -1,7 +1,28 @@
+'use client'
 import { MdSearch } from 'react-icons/md'
 import styles from './search.module.css'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
 
 const Search = ({placeholder}) => {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const {replace} = useRouter()
+
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(searchParams)
+
+    if( e.target.value){
+      e.target.value.length > 2 && params.set("q", e.target.value);
+    } else {
+      params.delete('q')
+    }
+    replace(`${pathname}?${params}`)
+
+  },300)
+  
+
+
   return (
     <div className={styles.container}>
         <MdSearch />
@@ -9,6 +30,7 @@ const Search = ({placeholder}) => {
             type='text' 
             placeholder={placeholder}
             className={styles.input}
+            onChange={handleSearch}
         />
     </div>
   )
